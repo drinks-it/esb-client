9e2b014e5a3d3c568e3321270edadfea26fd1ad2
Dima Volik <dima@drink.ch>
2023-04-18 15:35:38 +0200

    fix: added family to order.item request

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -318,6 +318,7 @@ final class ApiClient
                 'rowTotal' => $item->getRowTotal(),
                 'rowTotalInclTax' => $item->getRowTotalInclTax(),
                 'isSilverbogenProduct' => $item->isSilverbogenProduct(),
+                'family' => $item->getFamily(),
             ];
         }
         return $itemsData;
